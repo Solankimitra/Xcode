3650083e681928060768978fd5a47efdf2780501
Solankimitra <48064920+Solankimitra@users.noreply.github.com>
2020-01-23 07:20:25 +0000

    Add files via upload

---
+++ initialisation.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var str = "Hello, playground"
+
+class Person {
+    
+    var name  =  ""
+    var age = 0
+    
+    init(_ name:String , _ age:Int) {
+        //set up your object
+        self.name = name
+        self.age = age
+        
+    }
+    
+    
+    
+    
+}
+var a = Person("Solanki",26)
+
