8ba0abf91a85d4d008a58dfd205294566d139e57
Solankimitra <48064920+Solankimitra@users.noreply.github.com>
2020-01-23 06:51:34 +0000

    Add files via upload

---
+++ lesson2/lesson2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lesson2
+//
+//  Created by user161873 on 1/17/20.
+//  Copyright © 2020 user161873. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
